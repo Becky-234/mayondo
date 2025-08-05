2342fff2046bf83f3f9f3ed1ce486a4b8b85ce43
Becky-234 <kiraborebecca04@gmail.com>
2025-08-05 21:57:06 +0300

    conditions of logging in

---
+++ boolean.js
@@ -0,0 +1,5 @@
+let transportProvided = true;
+if (transportProvided) {
+    unitPrice = totalPrice * 0.5
+}
+console.log(transportProvided);
---
+++ index.js
@@ -0,0 +1,42 @@
+
+const isCorrectEmail = true;
+if (isCorrectEmail) {
+    console.log("Password")
+}
+
+const isCorrectPassword = true;
+if (isCorrectPassword) {
+    console.log("logging In");
+}
+
+
+const loginButton = true;
+if (loginButton) {
+    console.log("Welcome to the Dashboard!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const emailInput = document.getElementById("email");
+// const passwordInput = document.getElementById("password");
+// const loginBtn = document.getElementById("loginBtn");
