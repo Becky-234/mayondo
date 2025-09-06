c449ddf9f7dca73ae78eac08029565acbce9f2ab
Becky-234 <kiraborebecca04@gmail.com>
2025-09-06 15:43:12 +0300

    Created a public folder

--- server.js
+++ server.js
@@ -1,2 +1,31 @@
+//1.DEPENDENCIES
 const express = require('express');
+
+
+
+//2.INSTANTIATIONS
 const app = express();
+const port = 3000
+
+//3.CONFIGURATIONS
+
+
+//4.MIDDLEWARE
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));    //helps to pass data from forms
+
+
+//5.ROUTES
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
